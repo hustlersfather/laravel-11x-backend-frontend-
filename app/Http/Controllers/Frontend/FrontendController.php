8059fc28a02c0f69e66b7d9a349bae23eddf2b38
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2024-10-09 19:18:49 +1345

    Update FrontendController.php

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
 
 class FrontendController extends Controller
 {
     /**
      * Retrieves the view for the index page of the frontend.
      *
-     * @return \Illuminate\Contracts\View\View
+     * @return View
      */
-    public function index()
+    public function index(): View
     {
         return view('frontend.index');
     }
@@ -19,9 +20,9 @@ class FrontendController extends Controller
     /**
      * Privacy Policy Page.
      *
-     * @return \Illuminate\Contracts\View\View
+     * @return View
      */
-    public function privacy()
+    public function privacy(): View
     {
         return view('frontend.privacy');
     }
@@ -29,10 +30,110 @@ class FrontendController extends Controller
     /**
      * Terms & Conditions Page.
      *
-     * @return \Illuminate\Contracts\View\View
+     * @return View
      */
-    public function terms()
+    public function terms(): View
     {
         return view('frontend.terms');
+    }
+
+    /**
+     * Tutorial Page.
+     *
+     * @return View
+     */
+    public function tutorial(): View
+    {
+        return view('frontend.tutorial');
+    }
+
+    /**
+     * Reports Page.
+     *
+     * @return View
+     */
+    public function reports(): View
+    {
+        return view('frontend.reports');
+    }
+
+    /**
+     * Rules Page.
+     *
+     * @return View
+     */
+    public function rules(): View
+    {
+        return view('frontend.rules');
+    }
+
+    /**
+     * Static Page.
+     *
+     * @return View
+     */
+    public function static(): View
+    {
+        return view('frontend.static');
+    }
+
+    /**
+     * SMTP Page.
+     *
+     * @return View
+     */
+    public function smtp(): View
+    {
+        return view('frontend.smtp');
+    }
+
+    /**
+     * cPanel Page.
+     *
+     * @return View
+     */
+    public function cPanel(): View
+    {
+        return view('frontend.cPanel');
+    }
+
+    /**
+     * Shell Page.
+     *
+     * @return View
+     */
+    public function shell(): View
+    {
+        return view('frontend.shell');
+    }
+
+    /**
+     * Leads Page.
+     *
+     * @return View
+     */
+    public function leads(): View
+    {
+        return view('frontend.leads');
+    }
+
+    /**
+     * Premium Page.
+     *
+     * @return View
+     */
+    public function premium(): View
+    {
+        return view('frontend.premium');
+    }
+
+    /**
+     * Active Page.
+     *
+     * @return View
+     */
+    public function active(): View
+    {
+        return view('frontend.active');
     }
 }
